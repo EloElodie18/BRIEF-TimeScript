80b8ee5518af696f79eb265eae3e5766ee9a30e5
EloElodie18 <elodierichard@yahoo.fr>
2023-11-16 20:19:30 +0100

    level1

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+//LEVEL 1//
+
+//1st question
+let chaine: string = "Brief";
+let nombre: number = 2;
+let bool: boolean = true;
+let nombreavirgule: number = 2.5;
+let symbole: symbol = Symbol("truc");
+/*let grdNombre: bigint = 1n;*/
+let nul: null = null;
+let nondefini: undefined = undefined;
+console.log(
+  chaine,
+  " ",
+  nombre,
+  " ",
+  bool,
+  " ",
+  nombreavirgule,
+  " ",
+  symbole,
+  " ",
+  nul,
+  " ",
+  nondefini
+);
+
+//2nd question
+let fruits: string[] = ["pomme", "poire", "orange"]; /*ecrire scd maniere*/
+console.log(fruits);
+
+//3rd question
+let tout: any;
+console.log(tout);
+
+//4rd question
+function cool(x: number, y: number): number {
+  /*je peux me l'ecrire d'une autre manière*/
+  return x + y;
+}
+console.log(1, 1);
